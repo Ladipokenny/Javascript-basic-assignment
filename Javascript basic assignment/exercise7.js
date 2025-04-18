d9d89cdb4044c9a1280383bcea97b3bae73ef1c8
Kenny <111467286+Ladipokenny@users.noreply.github.com>
2025-04-18 23:29:29 +0100

    Update exercise7.js

--- Javascript basic assignment/exercise7.js
+++ Javascript basic assignment/exercise7.js
@@ -9,12 +9,12 @@ if (guess === randomNumber) {#
     alert('Wrong guess, The number was ${randomNumber}');
 }
 
-Sample Output
-First ans:
-Prompt:Guess a number between 1 and 5
-User input:2
-Alert: You are right! (if the random nunmber generated is 2)
-Second ans:
-Prompt:Guess a number between 1 and 5
-User input:2
-Alert: Wrong guess, The number was 3 (if the random nunmber generated is 3)
+//Sample Output
+//First ans:
+//Prompt:Guess a number between 1 and 5
+//User input:2
+//Alert: You are right! (if the random nunmber generated is 2)
+//Second ans:
+//Prompt:Guess a number between 1 and 5
+//User input:2
+//Alert: Wrong guess, The number was 3 (if the random nunmber generated is 3)
