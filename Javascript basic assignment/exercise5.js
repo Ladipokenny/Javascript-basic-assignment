324e581b921519edc5dd36dc82cdc10c36017ec6
Kenny <111467286+Ladipokenny@users.noreply.github.com>
2025-04-18 23:27:44 +0100

    Update exercise5.js

--- Javascript basic assignment/exercise5.js
+++ Javascript basic assignment/exercise5.js
@@ -6,18 +6,18 @@ for (let i = 1; i <= 12; i++) {
         {i} = ${number * i}');
 }
 
-Sample output:
-User Input:3
-Console Output:
-3 x  1=3
-3 x  2=6
-3 x  3=9
-3 x  4=12
-3 x  5=15
-3 x  6=18
-3 x  7=21
-3 x  8=24
-3 x  9=27
-3 x  10=30
-3 x  11=33
-3 x  12=36
+//Sample output:
+//User Input:3
+//Console Output:
+//3 x  1=3
+//3 x  2=6
+//3 x  3=9
+//3 x  4=12
+//3 x  5=15
+//3 x  6=18
+//3 x  7=21
+//3 x  8=24
+//3 x  9=27
+//3 x  10=30
+//3 x  11=33
+//3 x  12=36
